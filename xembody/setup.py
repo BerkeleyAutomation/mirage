370a42ab56ced2285ca452e3dc32a86a01e0ba57
Karthik Dharmarajan <kdharmarajan@berkeley.edu>
2024-02-27 04:23:11 +0000

    Updated docs

--- xembody/setup.py
+++ xembody/setup.py
@@ -8,7 +8,16 @@ root_dir = os.path.dirname(os.path.realpath(__file__))
 setup(
     name='xembody',
     version='0.1',
-    description='Repo for xembody',
+    description='Repo for Mirage',
+    author='Lawrence Chen, Karthik Dharmarajan, Kush Hari',
     package_dir = {'': '.'},
-    packages=find_packages(include=['xembody', 'xembody.*'])
+    packages=find_packages(include=['xembody', 'xembody.*']),
+    install_requires=[
+    ],
+    extra_requires={
+        "docs": [
+            "sphinx",
+            "pydata-sphinx-theme"
+        ]
+    }
 )
