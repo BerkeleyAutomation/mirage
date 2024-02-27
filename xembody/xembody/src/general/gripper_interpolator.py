6107582b8ba53be4614b1ec3c49e8376b5c08b6c
Karthik Dharmarajan <kdharmarajan@berkeley.edu>
2024-02-27 08:38:14 +0000

    Removed pointless gripper interpolator code

--- xembody/xembody/src/general/gripper_interpolator.py
+++ xembody/xembody/src/general/gripper_interpolator.py
@@ -52,10 +52,6 @@ class GripperInterpolator:
             print("Returning angles")
             return -1 * gripper_angles
         
-        # if self.source_robot == "Panda" and self.target_robot == "UR5e": 
-        # # TODO: add this for real gripper mapping
-        #     return -0.05702400673569841 * gripper_angles + 0.02670973458948458
-        
         relevant_interpoolator = self.interpolators[(self.source_robot, self.target_robot)]
         regression_model = LinearRegression()
 
